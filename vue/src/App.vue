4fa60bd892256a8d29cef4c5c1e2f947c96af890
daruken <dj.hongten@gmail.com>
2020-12-03 16:28:40 +0900

    Era project started.

---
+++ vue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Era msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Era from './components/Era.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Era
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/src/components/Era.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="era">
+    <div>
+      <h1 class="title">EPL 20/21</h1>
+    </div>
+    <hr>
+
+    <p>HongTen</p>
+
+    <div>
+      <ul v-for="(club, i) in clubs" :key="i" class="list-unstyled">
+        <li>
+          {{ i+1 }}. {{ club.name }} {{ club.stadium }}
+        </li>
+      </ul>
+    </div>
+
+  </div>
+</template>
+
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Era',
+
+  data() {
+    return {
+      title: 'club',
+      clubs: []
+    }
+  },
+  methods: {
+    getTodos () {
+        axios.get('http://localhost:28080/club')
+        .then((res) => {
+          this.clubs = res.data
+        })
+      },
+    },
+    mounted(){
+      this.getTodos()
+    }
+  }
+</script>
